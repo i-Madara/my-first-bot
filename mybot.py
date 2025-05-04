338f8d03f6ef8935e6d14cea7c95e39226e928fd
omar <dromartaha93@gmail.com>
2025-05-04 23:37:50 +0300

    Fix Filters import again

--- mybot.py
+++ mybot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 from telegram.ext import filters
 import os, logging
 
